57778a63540aee123d456d5e1c95ba59e3df5efe
FlorentPiette <130734975+FlorentPiette@users.noreply.github.com>
2023-09-28 09:12:48 +0200

    Add files via upload

---
+++ Connexion.php
@@ -0,0 +1,29 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<body>
+<form action="Administration.php" method="post">
+
+    <p>
+       Email :
+    </p>
+
+    <input type="text" name="Email">
+
+    <p>
+        Mot de passe :
+    </p>
+
+    <input type="text" name="MotDePasse"><br>
+
+    <input type="submit" value="Connexion">
+
+</form>
+
+</body>
+</html>
+<?php
