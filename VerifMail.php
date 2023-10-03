41bdb112380e65b03c2c76da990cd9e27a2dd02b
Kyll <Kyllian_jg971@hotmail.com>
2023-10-03 16:16:55 +0200

    Completion de L'inscription avec le Mail

---
+++ VerifMail.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Verification du Mail</title>
+</head>
+<body>
+<form action="ControllerVerifMail.php" method="post">
+    <input type="text" name="verification" placeholder=" Veuillez saisir le code reçu par mail">
+    <input type="submit" name="validation" >
+
+
+
+</form>
+
+</body>
+</html>
