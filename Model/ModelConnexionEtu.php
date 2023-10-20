52ff44849fb368e729d92010c60970c6a968b0b1
Emeline <emeline.delarue31@gmail.com>
2023-10-20 14:21:47 +0200

    Commentaires ModelConnexionEtu

--- Model/ModelConnexionEtu.php
+++ Model/ModelConnexionEtu.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Récuperer de la base de donnée, les email et mot de passes de tous les étudiants
+ *
+ * @param PDO $conn
+ *
+ * @return array $result
+ */
 function selectEmailMDPEtu($conn){
     $req = "SELECT email, motdepasse FROM Etudiant";
     $req2 = $conn->prepare($req);
@@ -9,6 +16,15 @@ function selectEmailMDPEtu($conn){
     return $result;
 }
 
+/**
+ * Vérifier si les identifiants correspondent à une personne de la base de donnée
+ *
+ * @param array $students
+ * @param string $email
+ * @param string $motDePasse
+ *
+ * @return boolean
+ */
 function authenticated ($students,$email,$motDePasse){
     foreach ($students as $student) {
         if ($student['email'] === $email && password_verify($student['motdepasse'],$motDePasse)) {
