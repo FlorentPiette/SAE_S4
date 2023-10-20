7e2d093c6be824705eaa6a35331685afc8db1579
Emeline <emeline.delarue31@gmail.com>
2023-10-20 14:25:58 +0200

    Commentaires ModelVerifMail

--- Model/ModelVerifMail.php
+++ Model/ModelVerifMail.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Récuperer de la base de donnée, le code de confirmation de l'étudiant qui a cette adresse email
+ *
+ * @param PDO $conn
+ * @param String $email
+ *
+ * @return array $result
+ */
 function selectCodeEtuWhereEmail($conn, $email){
     $req = "SELECT CodeConfirmation from Etudiant where email = ?";
     $req2 = $conn->prepare($req);
