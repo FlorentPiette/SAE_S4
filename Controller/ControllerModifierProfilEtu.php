7f0ed74a89c3c91a7470449c3109f22113eb5991
Emeline <emeline.delarue31@gmail.com>
2023-10-23 14:47:47 +0200

    Amélioration modification d'un profil

--- Controller/ControllerModifierProfilEtu.php
+++ Controller/ControllerModifierProfilEtu.php
@@ -4,46 +4,58 @@ include '../Model/ModelModifierProfilEtu.php';
 
 $conn = Conn::getInstance();
 
-$id = 1;
+$id = 5;
+$etu = selectEtudiant($conn, $id);
+
+include ("../View/ViewModifierProfilEtu.php");
 
 if (isset($_POST['modifier_nom'])){
     $nom = $_POST['nouveau_nom'];
     updateNomEtu($conn, $nom, $id);
+    // Rediriger l'utilisateur vers la page de profil de l'étudiant mis à jour
+    header("Location: ControllerModifierProfilEtu.php");
 }
 
 if (isset($_POST['modifier_prenom'])){
     $prenom = $_POST['nouveau_prenom'];
     updatePrenomEtu($conn, $prenom, $id);
+    header("Location: ControllerModifierProfilEtu.php");
 }
 
 if (isset($_POST['modifier_adresse'])){
     $adresse = $_POST['nouvelle_adresse'];
     updateAdresseEtu($conn, $adresse, $id);
+    header("Location: ControllerModifierProfilEtu.php");
 }
 
 if (isset($_POST['modifier_ville'])){
     $ville = $_POST['nouvelle_ville'];
     updateVilleEtu($conn, $ville, $id);
+    header("Location: ControllerModifierProfilEtu.php");
 }
 
 if (isset($_POST['modifier_cp'])){
     $codePostal = $_POST['nouveau_cp'];
     updateCpEtu($conn, $codePostal, $id);
+    header("Location: ControllerModifierProfilEtu.php");
 }
 
 if (isset($_POST['modifier_anneeEtude'])){
     $anneeEtude = $_POST['annouvelle_anneeEtudeneeetude'];
     updateAnneeEtudeEtu($conn, $anneeEtude, $id);
+    header("Location: ControllerModifierProfilEtu.php");
 }
 
 if (isset($_POST['modifier_formation'])){
     $formation = $_POST['nouvelle_formation'];
     updateFormationEtu($conn, $formation, $id);
+    header("Location: ControllerModifierProfilEtu.php");
 }
 
 if (isset($_POST['modifier_email'])){
     $email = $_POST['nouvel_email'];
     updateEmailEtu($conn, $email, $id);
+    header("Location: ControllerModifierProfilEtu.php");
 }
 
 if(isset($_POST["setActif"])){
@@ -53,8 +65,6 @@ if(isset($_POST["setActif"])){
     else{
         updateInactif($conn, $id);
     }
+    header("Location: ControllerModifierProfilEtu.php");
 }
-
-// Rediriger l'utilisateur vers la page de profil de l'étudiant mis à jour
-header("Location: ControllerAfficherProfilEtu.php");
 ?>
