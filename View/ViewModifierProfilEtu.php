367f2de150eb217df49677451e598bfa92c8d2ae
Emeline <emeline.delarue31@gmail.com>
2024-01-15 16:14:36 +0100

    Resolution de bugs

--- View/ViewModifierProfilEtu.php
+++ View/ViewModifierProfilEtu.php
@@ -5,10 +5,8 @@
     <title>Profil <?= $etu['nom'] ?> <?= $etu['prenom'] ?></title>
 
     <link rel="stylesheet" type="text/css" href="../asserts/css/ModifierProfilEtu.css">
-    <script src="../asserts/js/modifProfil.js"></script>
     <link rel="icon" href="../asserts/img/logo.png" type="image/x-icon">
-    <link rel="stylesheet" type="text/css" href="../asserts/css/Cloche.css">
-    <script src="../asserts/js/script.js"></script>
+
 </head>
 
 <body>
@@ -43,30 +41,7 @@
                         </div>
                     </li>
                     <li>
-                        <div class="notification">
-                            <div class="icon-bell" onclick="toggleNotifications()">
-                                <span class="badge" id="notificationBadge"> </span>
-                            </div>
-                        </div>
-                        <div class="burger-menu" id="burgerMenu" style="display: none;">
-                            <div class="millieu">
-                                <button type="button" id="showUnreadButton">Notifications non lues</button>
-                                <button type="button" id="showReadButton">Notifications lues</button>
-                            </div>
-
-                            <div>
-                                <h2 id="hnonlu">Notifications non lues</h2>
-                                <ul id="unreadNotificationList" ></ul>
-
-                            </div>
-                            <div>
-                                <h2 id="hlu">Notifications lues</h2>
-                                <ul id="readNotificationList"></ul>
-                            </div>
-
-                            <button type="button" id="validationButton" class="validationButton" ">Valider</button>
-
-                        </div>
+                        <a><img src="../asserts/img/notification.png" alt="Description de l'image" class="notification"></a>
                     </li>
                 </ul>
             </form>
@@ -99,120 +74,121 @@
 
 <div class="content">
 
+    <button type="submit" name="modifier_nom" value="Modifier" class="transparent-button">
+        <img class="editable" id="editIcon" src="../asserts/img/editer.png" alt="edit">
+    </button>
+    <button type="submit" name="annuler_nom" value="Annuler" class="transparent-button">
+        <img class="editable" id="annuleIcon" src="../asserts/img/annulerModif.png" alt="edit">
+    </button>
 
-<h1><?= $etu['nom'] ?> <?= $etu['prenom'] ?></h1>
-    <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
-    <label> Nom : </label>
-    <input type="text" name="nouveau_nom" value="<?= $etu['nom'] ?>">
-        <button type="submit" name="modifier_nom" value="Modifier" class="transparent-button">
-            <img class="editable" id="editIcon" src="../asserts/img/editer.png" alt="edit">
-        </button>
-</form>
 
-    <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
-    <label> Prénom : </label>
-    <input type="text" name="nouveau_prenom" value="<?= $etu['prenom'] ?>">
-        <button type="submit" name="modifier_prenom" value="Modifier" class="transparent-button">
-            <img class="editable" id="editIcon" src="../asserts/img/editer.png" alt="edit">
-        </button>
-</form>
+    <h1><?= $etu['nom'] ?> <?= $etu['prenom'] ?></h1>
+    <div class="modif">
+        <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
+            <div class="saisie1">
+                <label> Nom : </label>
+                <input type="text" name="nouveau_nom" value="<?= $etu['nom'] ?>">
+            </div>
+        </form>
 
-    <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
-    <label> Date de naissance : </label>
-    <input type="date" name="modifier_date" value="<?= $etu['datedenaissance'] ?>">
-        <button type="submit" name="modifier_date" value="Modifier" class="transparent-button">
-            <img class="editable" id="editIcon" src="../asserts/img/editer.png" alt="edit">
-        </button>
-</form>
+        <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
+            <div class="saisie2">
+                <label> Prénom : </label>
+                <input type="text" name="nouveau_prenom" value="<?= $etu['prenom'] ?>">
+            </div>
+        </form>
 
-    <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
-    <label> Adresse : </label>
-    <input type="text" name="nouvelle_adresse" value="<?= $etu['adresse'] ?>">
-        <button type="submit" name="modifier_adresse" value="Modifier" class="transparent-button">
-            <img class="editable" id="editIcon" src="../asserts/img/editer.png" alt="edit">
-        </button>
-</form>
+        <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
+            <div class="saisie1">
+                <label> Date de naissance : </label>
+                <input type="date" name="modifier_date" value="<?= $etu['datedenaissance'] ?>">
+            </div>
 
-    <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
-    <label> Ville : </label>
-    <input type="text" name="nouvelle_ville" value="<?= $etu['ville'] ?>">
-        <button type="submit" name="modifier_ville" value="Modifier" class="transparent-button">
-            <img class="editable" id="editIcon" src="../asserts/img/editer.png" alt="edit">
-        </button>
-</form>
 
-    <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
-    <label> Code postal : </label>
-    <input type="number" name="nouveau_cp" value="<?= $etu['codepostal'] ?>">
-        <button type="submit" name="modifier_cp" value="Modifier" class="transparent-button">
-            <img class="editable" id="editIcon" src="../asserts/img/editer.png" alt="edit">
-        </button>
-</form>
+        </form>
 
-    <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
-    <label> Année d'étude : </label>
-    <input type="number" name="nouvelle_anneeEtude" value="<?= $etu['anneeetude'] ?>">
-        <button type="submit" name="modifier_anneeEtude" value="Modifier" class="transparent-button">
-            <img class="editable" id="editIcon" src="../asserts/img/editer.png" alt="edit">
-        </button>
-</form>
+        <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
+            <div class="saisie2">
+                <label> Adresse : </label>
+                <input type="text" name="nouvelle_adresse" value="<?= $etu['adresse'] ?>">
+            </div>
+        </form>
 
-    <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
-    <label> Formation : </label>
-    <input type="text" name="nouvelle_formation" value="<?= $etu['formation'] ?>">
-        <button type="submit" name="modifier_formation" value="Modifier" class="transparent-button">
-            <img class="editable" id="editIcon" src="../asserts/img/editer.png" alt="edit">
-        </button>
-</form>
+        <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
+            <div class="saisie1">
+                <label> Ville : </label>
+                <input type="text" name="nouvelle_ville" value="<?= $etu['ville'] ?>">
+            </div>
+        </form>
 
-    <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
-    <label> Email : </label>
-    <input type="email" name="nouvel_email" value="<?= $etu['email'] ?>">
-        <button type="submit" name="modifier_email" value="Modifier" class="transparent-button">
-            <img class="editable" id="editIcon" src="../asserts/img/editer.png" alt="edit">
-        </button>
-</form>
+        <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
+            <div class="saisie2">
+                <label> Code postal : </label>
+                <input type="number" name="nouveau_cp" value="<?= $etu['codepostal'] ?>">
+            </div>
+        </form>
 
-    <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
-    <label> Type d'entreprises recherchées : </label>
-    <input type="text" name="nouveau_typeentreprise" value="<?= $etu['typeentreprise'] ?>">
-        <button type="submit" name="modifier_typeentreprise" value="Modifier" class="transparent-button">
-            <img class="editable" id="editIcon" src="../asserts/img/editer.png" alt="edit">
-        </button>
-</form>
+        <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
+            <div class="saisie1">
+                <label> Année d'étude : </label>
+                <input type="number" name="nouvelle_anneeEtude" value="<?= $etu['anneeetude'] ?>">
+            </div>
+        </form>
 
-    <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
-    <label> Type de missions recherchées : </label>
-    <input type="text" name="nouveau_typemission" value="<?= $etu['typemission'] ?>">
-        <button type="submit" name="modifier_typemission" value="Modifier" class="transparent-button">
-            <img class="editable" id="editIcon" src="../asserts/img/editer.png" alt="edit">
-        </button>
-</form>
+        <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
+            <div class="saisie2">
+                <label> Formation : </label>
+                <input type="text" name="nouvelle_formation" value="<?= $etu['formation'] ?>">
+            </div>
+        </form>
 
-    <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
-        <label for="mobile">
-            Mobile:
-            <select name="mobile" id="mobile">
-                <option value="10" <?php if ($etu['mobile'] == 10) echo 'selected'; ?>>10km</option>
-                <option value="50" <?php if ($etu['mobile'] == 50) echo 'selected'; ?>>50km</option>
-                <option value="100" <?php if ($etu['mobile'] == 100) echo 'selected'; ?>>100km</option>
-                <option value="500" <?php if ($etu['mobile'] == 500) echo 'selected'; ?>>500km</option>
-                <option value="1000" <?php if ($etu['mobile'] == 1000) echo 'selected'; ?>>1000km</option>
-                <option value="99999" <?php if ($etu['mobile'] == 99999) echo 'selected'; ?>>International</option>
-            </select>
-        </label>
-        <button type="submit" name="modifier_mobile" value="Modifier" class="transparent-button">
-            <img class="editable" id="editIcon" src="../asserts/img/editer.png" alt="edit">
-        </button>
-</form>
+        <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
+            <div class="saisie1">
+                <label> Email : </label>
+                <input type="email" name="nouvel_email" value="<?= $etu['email'] ?>">
+            </div>
+        </form>
 
-    <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
-    <label> Actif : </label>
-    <input type="checkbox" name="actif" id="actif" value="actif" <?php if ($etu['actif']) echo 'checked'; ?>>
-        <button type="submit" name="setActif" value="Modifier" class="transparent-button">
-            <img class="editable" id="editIcon" src="../asserts/img/editer.png" alt="edit">
-        </button>
-</form>
+        <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
+            <div class="saisie2">
+                <label> Type d'entreprises recherchées : </label>
+                <input type="text" name="nouveau_typeentreprise" value="<?= $etu['typeentreprise'] ?>">
+            </div>
+        </form>
+
+        <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
+            <div class="saisie1">
+                <label> Type de missions recherchées : </label>
+                <input type="text" name="nouveau_typemission" value="<?= $etu['typemission'] ?>">
+            </div>
+        </form>
+
+        <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
+            <div class="saisie2">
+                <label for="mobile">
+                    Mobile:
+                    <select name="mobile" id="mobile">
+                        <option value="10" <?php if ($etu['mobile'] == 10) echo 'selected'; ?>>10km</option>
+                        <option value="50" <?php if ($etu['mobile'] == 50) echo 'selected'; ?>>50km</option>
+                        <option value="100" <?php if ($etu['mobile'] == 100) echo 'selected'; ?>>100km</option>
+                        <option value="500" <?php if ($etu['mobile'] == 500) echo 'selected'; ?>>500km</option>
+                        <option value="1000" <?php if ($etu['mobile'] == 1000) echo 'selected'; ?>>1000km</option>
+                        <option value="99999" <?php if ($etu['mobile'] == 99999) echo 'selected'; ?>>International</option>
+                    </select>
+                </label>
+            </div>
+        </form>
+
+        <form method="post" action="../Controller/ControllerModifierProfilEtu.php<?php echo isset($_GET['ine']) ? '?ine=' . $_GET['ine'] : ''; ?>">
+            <div class="saisie1">
+                <label> Actif : </label>
+                <input type="checkbox" name="actif" id="actif" value="actif" <?php if ($etu['actif']) echo 'checked'; ?>>
+            </div>
+        </form>
+    </div>
 </div>
+<script src="../asserts/js/modifProfil.js"></script>
+
+
 </body>
 </html>
--- View/ViewModifierProfilPerso.php
+++ View/ViewModifierProfilPerso.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" type="text/css" href="../asserts/css/ModifierProfilPerso.css">
     <script src="../asserts/js/modifProfil.js"></script>
     <link rel="icon" href="../asserts/img/logo.png" type="image/x-icon">
+        <link rel="stylesheet" type="text/css" href="../asserts/css/Cloche.css">
+        <script src="../asserts/js/script.js"></script>
 </head>
 
 <body>
@@ -50,7 +52,30 @@
                         </div>
                     </li>
                     <li>
-                        <a><img src="../asserts/img/notification.png" alt="Description de l'image" class="notification"></a>
+                        <div class="notification">
+                            <div class="icon-bell" onclick="toggleNotifications()">
+                                <span class="badge" id="notificationBadge"> </span>
+                            </div>
+                        </div>
+                        <div class="burger-menu" id="burgerMenu" style="display: none;">
+                            <div class="millieu">
+                                <button type="button" id="showUnreadButton">Notifications non lues</button>
+                                <button type="button" id="showReadButton">Notifications lues</button>
+                            </div>
+
+                            <div>
+                                <h2 id="hnonlu">Notifications non lues</h2>
+                                <ul id="unreadNotificationList" ></ul>
+
+                            </div>
+                            <div>
+                                <h2 id="hlu">Notifications lues</h2>
+                                <ul id="readNotificationList"></ul>
+                            </div>
+
+                            <button type="button" id="validationButton" class="validationButton" ">Valider</button>
+
+                        </div>
                     </li>
                 </ul>
             </form>
