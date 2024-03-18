9f2bb38248ae1acd4d16fe9ea4e0bb9a99fbc9cf
tashoku <romain.person@uphf.fr>
2024-03-18 15:52:45 +0100

    Limite de taille de fichiers + contrainte de type .pdf

--- Controller/ControllerImportationCV.php
+++ Controller/ControllerImportationCV.php
@@ -16,15 +16,25 @@ include '../View/ViewImportationCV.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['Importer'])) {
         if (isset($_FILES["fichier"]) && $_FILES["fichier"]["error"] == UPLOAD_ERR_OK) {
+            $maxFileSize = 5 * 1024 * 1024;
+            $allowedFileTypes = ['application/pdf'];
 
-            $dossier_destination = "../Controller/";
-            $chemin_fichier_destination = $dossier_destination . basename($_FILES["fichier"]["name"]);
+            if ($_FILES["fichier"]["size"] > $maxFileSize) {
+                echo "Erreur : La taille du fichier est supérieure à la limite autorisée.";
+            } elseif (!in_array($_FILES["fichier"]["type"], $allowedFileTypes)) {
+                echo "Erreur : Seuls les fichiers PDF sont autorisés.";
+            } else {
+                $dossier_destination = "../Controller/";
+                $chemin_fichier_destination = $dossier_destination . basename($_FILES["fichier"]["name"]);
 
-            if (move_uploaded_file($_FILES["fichier"]["tmp_name"], $chemin_fichier_destination)) {
-                ajouterCV($db, $id, $chemin_fichier_destination);
+                if (move_uploaded_file($_FILES["fichier"]["tmp_name"], $chemin_fichier_destination)) {
+                    ajouterCV($db, $id, $chemin_fichier_destination);
+                } else {
+                    echo "Erreur lors du déplacement du fichier vers le serveur.";
+                }
             }
         } else {
-            echo "Erreur lors du déplacement du fichier vers le serveur.";
+            echo "Erreur lors du téléchargement du fichier.";
         }
     }
     if (isset($_POST['NePasImporter'])){
